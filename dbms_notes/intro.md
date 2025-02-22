1df01438ed268b477c95d9d90845467223df0c7b
lakshyarawat1 <lakshya.paramount@gmail.com>
2025-02-22 16:03:21 +0530

    [ Addition ]

---
+++ dbms_notes/intro.md
@@ -0,0 +1,14 @@
+# Database Management System (DBMS) Notes
+
+## Introduction
+
+### Data
+
+- Collection of raw bytes, facts and details.
+- Data is meaningless.
+
+### Information
+
+- Data that has been processed.
+- Information is meaningful.
+
---
+++ hash/containsDuplicate.cpp
@@ -0,0 +1,23 @@
+// The "Contains Duplicate" problem on LeetCode is a common algorithmic challenge.
+// The problem statement is as follows:
+
+// Given an integer array `nums`, return `true` if any value appears at least twice in the array,
+// and return `false` if every element is distinct.
+
+// Example:
+// Input: nums = [1,2,3,1]
+// Output: true
+
+// Input: nums = [1,2,3,4]
+// Output: false
+
+// Input: nums = [1,1,1,3,3,4,3,2,4,2]
+// Output: true
+
+// The goal is to determine if there are any duplicate elements in the array.
+// There are multiple ways to solve this problem, including using sorting or a hash set.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
